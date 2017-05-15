28600b98044a27d11a3cbcf243702d1060193071
YongKiat <ytay024@e.ntu.edu.sg>
2017-05-15 10:07:18 +0800

    Remove unused files

---
+++ build/CMakeFiles/UnscentedKF.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/ChingYee/Documents/Udacity/SDC_term2/CarND-Unscented-Kalman-Filter-Project/src/main.cpp" "C:/Users/ChingYee/Documents/Udacity/SDC_term2/CarND-Unscented-Kalman-Filter-Project/build/CMakeFiles/UnscentedKF.dir/main.cpp.o"
+  "C:/Users/ChingYee/Documents/Udacity/SDC_term2/CarND-Unscented-Kalman-Filter-Project/src/tools.cpp" "C:/Users/ChingYee/Documents/Udacity/SDC_term2/CarND-Unscented-Kalman-Filter-Project/build/CMakeFiles/UnscentedKF.dir/tools.cpp.o"
+  "C:/Users/ChingYee/Documents/Udacity/SDC_term2/CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp" "C:/Users/ChingYee/Documents/Udacity/SDC_term2/CarND-Unscented-Kalman-Filter-Project/build/CMakeFiles/UnscentedKF.dir/ukf.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/UnscentedKF.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnscentedKF.dir/ukf.cpp.o"
+  "CMakeFiles/UnscentedKF.dir/main.cpp.o"
+  "CMakeFiles/UnscentedKF.dir/tools.cpp.o"
+  "UnscentedKF.pdb"
+  "UnscentedKF.exe"
+  "UnscentedKF.exe.manifest"
+  "libUnscentedKF.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UnscentedKF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
